4f79ffedecaa224793b3c5239aaf8919f5ef7780
Roman Manzhula <roman1985al@ukr.net>
2024-12-23 23:25:30 +0200

    Update TestAnnotation.java
    In Ukrainian

--- PA_Annotation_example_01.02.2023/src/TestAnnotation.java
+++ PA_Annotation_example_01.02.2023/src/TestAnnotation.java
@@ -2,25 +2,24 @@
 
 import java.lang.annotation.*;
 
-@Inherited //говорит о том, что новая созданная аннотация будет передаваться по наследству
-@Target(value = {ElementType.TYPE, ElementType.FIELD, ElementType.METHOD}) //указывает к каким членам класса мы можем
-// аннтоцацию применять, для типа, поля, метода.
-//@Target(value = ElementType.METHOD) //указывает что аннотация применяется только к методу
-//что можно аннотировать ElementType:
-//ANNOTATION_TYPE - аннотации
-//CONSTRUCTOR - конструкторы
+@Inherited // вказує на те, що новостворена анотація буде передаватися по спадщині
+@Target(value = {ElementType.TYPE, ElementType.FIELD, ElementType.METHOD}) // вказує, до яких елементів класу можна застосовувати анотацію: типи, поля, методи.
+//@Target(value = ElementType.METHOD) // вказує, що анотація застосовується лише до методів
+//що можна анотуючи ElementType:
+//ANNOTATION_TYPE - анотації
+//CONSTRUCTOR - конструктори
 //FIELD - поля
-//LOCAL_VARIABLE - локальные переменные
-//METHODS - методы
-//PACKAGE - пакеты (очень редкая ситуация)
-//PARAMETER - параметры
-//TYPE - типы (клас, интерфейс, энум)
+//LOCAL_VARIABLE - локальні змінні
+//METHODS - методи
+//PACKAGE - пакети (дуже рідкісна ситуація)
+//PARAMETER - параметри
+//TYPE - типи (класи, інтерфейси, енуми)
 
-@Retention(value = RetentionPolicy.RUNTIME)// тип аннотации, в данном случае РАНТАЙМ (это значит она будет доступна для Рефлексии)
-//RetentionPolicy.CLASS - для сторонних инструментов
-//RetentionPolicy.RUNTIME - доступный для Рефлексии
-//RetentionPolicy.SOURCE - для компилятора
+@Retention(value = RetentionPolicy.RUNTIME) // тип анотації, в даному випадку РАНТАЙМ (це означає, що вона буде доступна для рефлексії)
+//RetentionPolicy.CLASS - для сторонніх інструментів
+//RetentionPolicy.RUNTIME - доступна для рефлексії
+//RetentionPolicy.SOURCE - для компілятора
 
 public @interface TestAnnotation {
-    String parameter1();
+    String parameter1(); // параметр анотації
 }
